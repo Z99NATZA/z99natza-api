a4418f0d301ded78269722510e529feb322db17c
Z99NATZA <z99natza@gmail.com>
2025-12-07 21:43:34 +0700

    write .json history chat

--- src/handle/ai/chat.rs
+++ src/handle/ai/chat.rs
@@ -7,10 +7,13 @@ use hyper::Request;
 use hyper::StatusCode;
 use hyper::body::Incoming;
 use serde::Deserialize;
+use serde::Serialize;
 use crate::app::AppResult;
 use crate::app::state::AppState;
 use crate::http::ToResponse;
-use crate::http::request::json;
+use crate::http::request;
+use tokio::fs;
+use std::path::Path;
 
 #[derive(Debug, Deserialize)]
 struct ChatRequest {
@@ -18,15 +21,49 @@ struct ChatRequest {
     sender: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct Chat {
+    sender: String,
+    message: String,
+}
+
 pub async fn chat_handle(
     state: Arc<AppState>,
     req: Request<Incoming>
 ) -> AppResult<Response<Full<Bytes>>> {
-    let chat_req: ChatRequest = json(req).await?;
+    let session_id = "ddd31a130".to_string();
+    
+    let chat_req: ChatRequest = request::json(req).await?;
     let input = chat_req.message.as_str();
-    println!("{:?}", chat_req);
+    
+    let chat_data = Chat {
+        sender: chat_req.sender.clone(),
+        message: chat_req.message.clone(),
+    };
+    
+    let json_file_name = format!("chat_{}.json", session_id);
+    let json_path = format!("data/chat_history/{}", json_file_name);
+    
+    let mut history: Vec<Chat> = if Path::new(&json_path).exists() {
+        let content = fs::read_to_string(&json_path).await?;
+        serde_json::from_str(&content)?
+    } else {
+        Vec::new()
+    };
+    
+    history.push(chat_data);
     
     let ai_message = state.ai.chat(input).await?;
+    
+    let chat_data = Chat {
+        sender: "ai".to_string(),
+        message: ai_message.clone(),
+    };
+    
+    history.push(chat_data);
+    
+    let json = serde_json::to_string_pretty(&history)?;
+    tokio::fs::write(&json_path, json).await?;
 
     #[derive(serde::Serialize)]
     struct ChatMessage {
